68f78ae68cb210d154647b1a8bf06ca33b80f375
Rachel Padworski <rachelpadworski@gmail.com>
2021-02-28 15:36:03 -0700

    iinitial commit & adds title & test photo

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('testing')
+// Unsplash API
+const count = 10;
+const apiKey = 'hPJ9g9-_AXDGL7JYEy_jZgFpoAgBz40CJOo_j5tBDTM';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
+
+// Get photos from Unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+// On load
+getPhotos();
